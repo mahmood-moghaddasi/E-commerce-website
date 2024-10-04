306e5b3d7e3adf2720ca6f2fa60796613c4b874d
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-10-04 11:12:14 +0330

    HomePage products created

---
+++ src/components/HomeProducts.jsx
@@ -0,0 +1,74 @@
+import styles from "./HomeProducts.module.css";
+
+function HomeProducts() {
+  const products = [
+    {
+      title: "Syltherine",
+      description: "Stylish cafe chair",
+      image: "../../public/images/products/sylther-product.png",
+      price: "2.500.00",
+    },
+    {
+      title: "Leviosa",
+      description: "Stylish cafe chair",
+      image: "../../public/images/products/leviosa-product.png",
+      price: "2.500.00",
+    },
+    {
+      title: "Lolito",
+      description: "Luxury big sofa",
+      image: "../../public/images/products/lolito-product.png",
+      price: "7.000.000",
+    },
+    {
+      title: "Respira",
+      description: "Outdoor bar table and stool",
+      image: "../../public/images/products/respira-product.png",
+      price: "500.000",
+    },
+    {
+      title: "Grifo",
+      description: "Night lamp",
+      image: "../../public/images/products/grifo-product.png",
+      price: "1.500.000",
+    },
+    {
+      title: "Muggo",
+      description: "Luxury small sofa",
+      image: "../../public/images/products/Muggo-product.png",
+      price: "4.000.000",
+    },
+    {
+      title: "Pingky",
+      description: "Luxury big sofa",
+      image: "../../public/images/products/pingky-product.png",
+      price: "7.000.000",
+    },
+    {
+      title: "Potty",
+      description: "Minimall sofa",
+      image: "../../public/images/products/potty-product.png",
+      price: "2.000.000",
+    },
+  ];
+  return (
+    <div className={styles.container}>
+      <h1>Our Products</h1>
+      <div className={styles.cards}>
+        {products.map((product, index) => (
+          <div className={styles.card} key={index}>
+            <img src={product.image} alt={product.title} />
+            <div className={styles.detailes}>
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <span>Rp {product.price}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button>Show More</button>
+    </div>
+  );
+}
+
+export default HomeProducts;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeBanner from "../components/HomeBanner";
 import HomeCategorys from "../components/HomeCategorys";
+import HomeProducts from "../components/HomeProducts";
 
 function HomePage() {
   return (
     <>
       <HomeBanner />
       <HomeCategorys />
+      <HomeProducts />
     </>
   );
 }
